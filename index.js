fb3d9e0652b001904450177e2b24276a3c751584
Isabella Hannah <isabellahannah735@gmail.com>
2019-06-05 12:47:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function breakOut(array, changeValue, stopValue){
   doSomethingToString(array[i]);
 }
 }
+
+// describe('loops', function() {
+  describe('breakOut(array, changeValue, stopValue)', function() {
+    it('changes every element in `array` to `changeValue` until it reachs `stopValue`', function() {
+      const c = Math.floor(Math.random() * 100)
+      expect(breakOut([1, 1, 1, 1, 2, 1, 1], c, 2)).toEqual([
+        c, c, c, c, 2, 1, 1
+      ])
+    }) 
+  })
+  /
