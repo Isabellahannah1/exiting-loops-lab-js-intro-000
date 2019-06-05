74a479748496303141d87783cb838de2b3cb9a03
Isabella Hannah <isabellahannah735@gmail.com>
2019-06-05 12:59:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function breakOut(array, changeValue, stopValue) {
+/* function breakOut(array, changeValue, stopValue) {
   for (var i = 0; i < array.length; i++) {
   if (array[i] === stopValue) {
     break;
@@ -10,5 +10,6 @@ return array;
 }
 
 function keepGoing(array, changeValue, skipValue){
-  
-}
+  for (var i = 0; i < array.length; i++) {
+    
+} */
