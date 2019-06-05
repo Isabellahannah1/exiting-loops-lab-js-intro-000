f2aa9d6a0e3e1d7ba003f81223e6e8c8c77e2c54
Isabella Hannah <isabellahannah735@gmail.com>
2019-06-05 12:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-function breakOut(array, changeValue, stopValue){}
+function breakOut(array, changeValue, stopValue){
+  for (let i = 0; i < array.length; i++) {
+  if (typeof array[i] !== 'string') {
+    break;
+  }
+ 
+  doSomethingToString(array[i]);
+}
+}
